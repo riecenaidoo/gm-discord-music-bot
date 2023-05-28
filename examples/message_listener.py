8c401226a3fe5805ef8826dbe72aa975b727f60b
riecenaidoo <riecenaidoo@outlook.com>
2023-05-28 21:26:41 +0200

    Refactoring Scripts
    [DRY] Extracted token grabbing methods from message_listener.py into main.py.
    
    <i>This lets us pass the token as a parameter in the run() functions of our example scripts, and not have to repeat the code.</i>

--- examples/message_listener.py
+++ examples/message_listener.py
@@ -1,34 +1,13 @@
 """
-Source: https://discordpy.readthedocs.io/en/latest/intro.html#basic-concepts
-
-discord.py revolves around the concept of events. 
-An event is something you listen to and then respond to. 
-For example, when a message happens, 
-you will receive an event about it that you can respond to.
-
-A quick example to showcase how events work:
+Listens to all messages in the Discord Server,
+displaying them out in the terminal.
 """
 
-import os
 
 import discord
 
 
-# Helper Methods
-
-def get_token(token_file):
-    """Open and read the file containing the token,
-    which should be on a single line."""
-
-    try:
-        with open(token_file, encoding="utf-8") as f:
-            return f.read()
-    except FileNotFoundError:
-        print(f"WARNING: The specified token file '{token_file}' was not found.")
-
-
 # This example requires the 'message_content' intent.
-
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
@@ -46,18 +25,9 @@ class MyClient(discord.Client):
         print(f'Message from {message.author}: {message.content}')
 
 
-def run():
+def run(token):
     intents = discord.Intents.default()
     intents.message_content = True
 
     client = MyClient(intents=intents)
-
-    """
-    Token controls access to the bot.
-    For safety, it is stored and read from a file that
-    does not get pushed to the repo, and is only saved
-    locally.
-    """
-    token = get_token(os.path.join("config", "token.txt"))
-    if token:
-        client.run(token)
+    client.run(token)
--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 """
-Dummy File
+Script Executions
 """
+import os
 
 from examples import message_listener
 
+
+# Helper Methods
+
+def get_token(token_path):
+    """Open and read the file containing the token,
+    which should be on a single line."""
+
+    try:
+        with open(token_path, encoding="utf-8") as f:
+            return f.read()
+    except FileNotFoundError:
+        print(f"[WARNING] The specified token file at '{token_path}' was not found.")
+
+
 if __name__ == "__main__":
-    message_listener.run()
+    """
+    Token controls access to the bot.
+    For safety, it is stored and read from a file that
+    does not get pushed to the repo, and is only saved
+    locally.
+    """
+    token = get_token(os.path.join("config", "token.txt"))
+    if token:
+        message_listener.run(token)
