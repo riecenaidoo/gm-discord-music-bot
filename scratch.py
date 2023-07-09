52456008aceb8a652474377bc91fc24eba577c5b
riecenaidoo <riecenaidoo@outlook.com>
2023-07-09 15:10:21 +0200

    dynamically import a module and run its function

--- scratch.py
+++ scratch.py
@@ -2,7 +2,14 @@ import os.path
 import pkgutil
 
 import examples
+import main
 
 pkg_path = os.path.dirname(examples.__file__)
 snippets = ([name for _, name, _ in pkgutil.iter_modules([pkg_path])])
 print(snippets)
+
+s = snippets[0]
+print(f"examples.{snippets[0]}")
+
+func = __import__(f"examples.{snippets[0]}", fromlist=["examples"])
+func.run(main.get_token(os.path.join("config", "token.txt")))
