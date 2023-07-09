d91a6bd9fb2f59791d02259276985a5d8335a550
riecenaidoo <riecenaidoo@outlook.com>
2023-07-09 17:34:10 +0200

    correctly labelled but binding calling the wrong functions

--- scratch.py
+++ scratch.py
@@ -1,15 +1,31 @@
 import os.path
 import pkgutil
 
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+
 import examples
 import main
 
-pkg_path = os.path.dirname(examples.__file__)
-snippets = ([name for _, name, _ in pkgutil.iter_modules([pkg_path])])
-print(snippets)
+token = main.get_token(os.path.join("config", "token.txt"))
 
-s = snippets[0]
-print(f"examples.{snippets[0]}")
 
-func = __import__(f"examples.{snippets[0]}", fromlist=["examples"])
-func.run(main.get_token(os.path.join("config", "token.txt")))
+class ExampleSelector(App):
+
+    def build(self):
+        pkg_path = os.path.dirname(examples.__file__)
+        snippets = ([name for _, name, _ in pkgutil.iter_modules([pkg_path])])
+        grid = GridLayout(rows=len(snippets))
+
+        for snippet in snippets:
+            button = Button(text=f"{snippet}")
+            func = __import__(f"examples.{snippet}", fromlist=["examples"])
+            button.bind(on_release=lambda x: func.run(token))
+            grid.add_widget(button)
+
+        return grid
+
+
+if __name__ == '__main__':
+    ExampleSelector().run()
