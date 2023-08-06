966d875b542d20e0dce26b6a5914e7bbb5ab295e
riecenaidoo <riecenaidoo@outlook.com>
2023-08-06 18:27:34 +0200

    messy but works

--- console.py
+++ console.py
@@ -4,89 +4,90 @@ TODO: Testing, Documentation, Singleton Implementation of Console.
 """
 
 
-
-
 class Command:
 
-	def __init__(self, nameMatch:str):
-		self.nameMatch = nameMatch
-		self.func = None
+    def __init__(self, name_match: str):
+        self.name_match = name_match.strip().casefold()
+        self.func = None
+
+    def set_action(self, action: callable):
+        self.func = action
+
+    def match(self, arg: str) -> bool:
+        return arg.strip().casefold() == self.name_match
+
+    def call(self, args: list[str]):
+        self.func(args)
 
 
-	def set_action(action: callable):
-		self.func = func
+def get_input() -> list[str]:
+    instruction = ""
+    while len(instruction) == 0:
+        instruction = input(" > ")
 
+    return instruction.split(" ")
 
-	def match(arg:str) -> bool:
-		return (arg == nameMatch)
-
-
-	def call(args:list(str)):
-		self.func(args)
-
-	
 
 class Console:
 
-	def __init__(self):
-		self.commands = list()
+    def __init__(self):
+        self.commands = list()
+
+    def add_command(self, command: Command):
+        self.commands.append(command)
+
+    def handle_command(self, args: list[str]):
+        """TODO: Raise exceptions"""
+
+        for cmd in self.commands:
+            if cmd.match(args[0]):
+                cmd.call(args)
+                break
+        else:
+            print("No match")
+
+    def run(self):
+        """TODO: Catch exceptions, end loop, display messages."""
+
+        while True:
+            try:
+                self.handle_command(get_input())
+            except UsageError as e:
+                print(f"Usage Error: {e.args[0]}")
 
 
-	def add_command(command:Command):
-		self.commands.append(command)
-
-
-	def get_input() -> list(str):
-		instruction = ""
-		while len(instruction) == 0:
-			instruction = input(" > ")
-
-		return instruction.split(" ")
-
-
-	def handle_command(args: list(str)):
-		"""TODO: Raise exceptions"""
-
-		for cmd in self.commands:
-			if cmd.match(args[0]):
-				cmd.call(args)
-				break
-		else:
-			print("No match")
-
-
-	def run():
-		"""TODO: Catch exceptions, end loop, display messages."""
-
-		while True:
-			handle_command(get_input())
+class UsageError(Exception):
+    pass
 
 
 if __name__ == '__main__':
-	console = Console()
+    console = Console()
 
-	play = Command("play")
-	play.set_action(lambda: print("play"))
-	console.add_command(play)
+    play_cmd = Command("play")
+    play_cmd.set_action(lambda args: print("[PLAY]"))
+    console.add_command(play_cmd)
 
-	volume = Command("volume")
-	def vol(args: list(str)):
-		if len(args) < 1:
-			raise UsageError("volume expects an argument")
-
-		volume = args[1]
-
-		try:
-			volume = int(volume)
-		except ValueError:
-			raise UsageError("volume argument must be integer")
-
-		if volume < 0 or volume > 100:
-			raise UsageError("volume must be between 0 & 100")
-
-		print("[VOLUME]")
-
-	volume.set_action(vol)
+    volume_cmd = Command("volume")
 
 
-	console.run()
+    def vol(args: list[str]):
+        if len(args) < 2:
+            raise UsageError("volume expects an argument")
+
+        volume = args[1]
+
+        try:
+            volume = int(volume)
+        except ValueError:
+            raise UsageError("volume argument must be integer")
+
+        if volume < 0 or volume > 100:
+            raise UsageError("volume must be between 0 & 100")
+
+        print("[VOLUME]")
+
+
+    volume_cmd.set_action(vol)
+    console.add_command(volume_cmd)
+
+    console.run()
