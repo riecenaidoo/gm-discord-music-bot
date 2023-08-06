b5ae9f408c97dd64f306ca67bfeaf6f16c666e2b
riecenaidoo <riecenaidoo@outlook.com>
2023-08-06 18:44:53 +0200

    cleaned up the implementation details

--- console.py
+++ console.py
@@ -6,21 +6,46 @@ TODO: Testing, Documentation, Singleton Implementation of Console.
 
 class Command:
 
-    def __init__(self, name_match: str):
+    def __init__(self, name_match: str, action_func: callable):
         self.name_match = name_match.strip().casefold()
-        self.func = None
-
-    def set_action(self, action: callable):
-        self.func = action
+        self.action_func = action_func
 
     def match(self, arg: str) -> bool:
         return arg.strip().casefold() == self.name_match
 
     def call(self, args: list[str]):
-        self.func(args)
+        self.action_func()
 
 
-def get_input() -> list[str]:
+class PlayCommand(Command):
+
+    def call(self, args: list[str]):
+        if len(args) < 2:
+            raise UsageError(f"{self.name_match} expects an argument")
+
+        self.action_func(args[1])
+
+
+class VolumeCommand(Command):
+
+    def call(self, args: list[str]):
+        if len(args) < 2:
+            raise UsageError(f"{self.name_match} expects an argument")
+
+        volume = args[1]
+
+        try:
+            volume = int(volume)
+        except ValueError:
+            raise UsageError("volume argument must be integer")
+
+        if volume < 0 or volume > 100:
+            raise UsageError("volume must be between 0 & 100")
+
+        self.action_func(volume)
+
+
+def get_console_input() -> list[str]:
     instruction = ""
     while len(instruction) == 0:
         instruction = input(" > ")
@@ -30,8 +55,9 @@ def get_input() -> list[str]:
 
 class Console:
 
-    def __init__(self):
+    def __init__(self, input_method: callable):
         self.commands = list()
+        self.input_method = input_method
 
     def add_command(self, command: Command):
         self.commands.append(command)
@@ -51,7 +77,7 @@ class Console:
 
         while True:
             try:
-                self.handle_command(get_input())
+                self.handle_command(self.input_method())
             except UsageError as e:
                 print(f"Usage Error: {e.args[0]}")
 
@@ -61,33 +87,9 @@ class UsageError(Exception):
 
 
 if __name__ == '__main__':
-    console = Console()
+    console = Console(get_console_input)
 
-    play_cmd = Command("play")
-    play_cmd.set_action(lambda args: print("[PLAY]"))
-    console.add_command(play_cmd)
-
-    volume_cmd = Command("volume")
-
-
-    def vol(args: list[str]):
-        if len(args) < 2:
-            raise UsageError("volume expects an argument")
-
-        volume = args[1]
-
-        try:
-            volume = int(volume)
-        except ValueError:
-            raise UsageError("volume argument must be integer")
-
-        if volume < 0 or volume > 100:
-            raise UsageError("volume must be between 0 & 100")
-
-        print("[VOLUME]")
-
-
-    volume_cmd.set_action(vol)
-    console.add_command(volume_cmd)
+    console.add_command(PlayCommand("play", lambda args: print(f"[PLAY] {args}")))
+    console.add_command(VolumeCommand("volume", lambda args: print(f"[PLAY] {args}")))
 
     console.run()
