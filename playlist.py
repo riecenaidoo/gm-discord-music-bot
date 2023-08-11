f34dc32c6d494988099a69803c6291e1bda2f7b3
riecenaidoo <riecenaidoo@outlook.com>
2023-08-11 10:51:28 +0200

    added Skeleton code - add module playlist.py to place MusicQueue class - added skeleton of the class - amended shuffle, loop and repeat modes to not need bools passed to them.

---
+++ playlist.py
@@ -0,0 +1,26 @@
+"""Tools for managing the playlist of the bot."""
+
+
+class MusicQueue:
+    pass
+
+    def add(self, url: str):
+        pass
+
+    def next(self) -> str:
+        pass
+
+    def prev(self) -> str:
+        pass
+
+    def shuffle(self):
+        pass
+
+    def loop(self):
+        pass
+
+    def repeat(self):
+        pass
+
+    def clear(self):
+        pass
