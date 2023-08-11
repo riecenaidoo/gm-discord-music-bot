eebc6464fc627de94b998e5b8f7cc0e63af5ac37
Riece Naidoo <125825660+riecenaidoo@users.noreply.github.com>
2023-08-11 15:22:29 +0200

    Music Queue (#37)
    Implemented a custom MusicQueue class to manage the playlist of the bot.

---
+++ playlist.py
@@ -0,0 +1,95 @@
+"""Tools for managing the playlist of the bot."""
+
+import random
+from enum import Enum
+
+
+class PlaylistMode(Enum):
+    SEQUENCE = 0
+    SHUFFLE = 1
+    LOOP = 2
+    REPEAT = 3
+
+
+class MusicQueue:
+
+    def __init__(self):
+        self.playlist = list()
+        self.recently_played = list()
+        self.mode = PlaylistMode(PlaylistMode.SEQUENCE)
+
+    def add(self, url: str):
+        self.playlist.append(url)
+
+    def next(self) -> str:
+        """
+        throws: ExhaustedException if there is no next element.
+        """
+        if self.mode == PlaylistMode.SEQUENCE:
+            if len(self.playlist) <= 0:
+                raise ExhaustedException
+            song = self.playlist.pop(0)
+            self.recently_played.append(song)
+            return song
+        elif self.mode == PlaylistMode.SHUFFLE:
+            if len(self.playlist) <= 0:
+                raise ExhaustedException
+            i = random.randrange(0, len(self.playlist))
+            song = self.playlist.pop(i)
+            self.recently_played.append(song)
+            return song
+        elif self.mode == PlaylistMode.LOOP:
+            if len(self.playlist) <= 0:
+                for song in self.recently_played:
+                    self.playlist.append(song)
+                self.recently_played.clear()
+            song = self.playlist.pop(0)
+            self.recently_played.append(song)
+            return song
+
+        elif self.mode == PlaylistMode.REPEAT:
+            return self.playlist[0]
+
+    def prev(self) -> str:
+        """
+        throws: ExhaustedException if there is no prev element.
+        """
+        if len(self.recently_played) <= 0:
+            raise ExhaustedException
+        # The last played song would have been appended to the end
+        return self.recently_played.pop(len(self.recently_played) - 1)
+
+    def normal(self):
+        self.mode = PlaylistMode(PlaylistMode.SEQUENCE)
+
+    def shuffle(self):
+        self.mode = PlaylistMode(PlaylistMode.SHUFFLE)
+
+    def loop(self):
+        self.mode = PlaylistMode(PlaylistMode.LOOP)
+
+    def repeat(self):
+        self.mode = PlaylistMode(PlaylistMode.REPEAT)
+
+    def clear(self):
+        self.playlist.clear()
+        self.recently_played.clear()
+
+
+class ExhaustedException(Exception):
+    pass
+
+
+if __name__ == '__main__':
+    # Manually confirming that #shuffle() works.
+    p = MusicQueue()
+    p.add("a")
+    p.add("b")
+    p.add("c")
+    p.add("d")
+    p.add("e")
+    p.shuffle()
+    print(p.next())
+    print(p.next())
+    print(p.next())
+    print(p.next())
---
+++ tests/test_playlist.py
@@ -0,0 +1,64 @@
+import unittest
+
+from playlist import MusicQueue, ExhaustedException
+
+
+class TestMusicQueue(unittest.TestCase):
+
+    def test_normal_next(self):
+        playlist = MusicQueue()
+        playlist.add("a")
+        playlist.add("b")
+        playlist.add("c")
+
+        self.assertEqual(playlist.next(), "a")
+        self.assertEqual(playlist.next(), "b")
+        self.assertEqual(playlist.next(), "c")
+        try:
+            playlist.next()
+            self.fail("Should throw an error!")
+        except ExhaustedException:
+            pass
+
+    def test_loop_next(self):
+        playlist = MusicQueue()
+        playlist.add("a")
+        playlist.add("b")
+        playlist.add("c")
+        playlist.loop()
+
+        self.assertEqual(playlist.next(), "a")
+        self.assertEqual(playlist.next(), "b")
+        self.assertEqual(playlist.next(), "c")
+        self.assertEqual(playlist.next(), "a")
+        self.assertEqual(playlist.next(), "b")
+        self.assertEqual(playlist.next(), "c")
+
+    def test_repeat_next(self):
+        playlist = MusicQueue()
+        playlist.add("a")
+        playlist.add("b")
+        playlist.add("c")
+        playlist.repeat()
+
+        self.assertEqual(playlist.next(), "a")
+        self.assertEqual(playlist.next(), "a")
+        self.assertEqual(playlist.next(), "a")
+
+    def test_prev(self):
+        playlist = MusicQueue()
+        playlist.add("a")
+        playlist.next()
+        self.assertEqual(playlist.prev(), "a")
+
+        playlist.add("b")
+        playlist.add("c")
+        playlist.next()
+        playlist.next()
+        self.assertEqual(playlist.prev(), "c")
+        self.assertEqual(playlist.prev(), "b")
+        try:
+            playlist.prev()
+            self.fail("Should throw an error!")
+        except ExhaustedException:
+            pass
