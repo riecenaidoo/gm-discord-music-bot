4abd5c60de8aadada2940cdb11a5824a4be1298f
Riece Naidoo <125825660+riecenaidoo@users.noreply.github.com>
2023-08-31 20:50:08 +0200

    Simplified the MusicClient (#56)
    Extracted responsibilities from the MusicClient.
    
    - extracted the building and running of the Console from the
    MusicClient.
    - created a single entry point to the event loop for the MusicClient and
    Console in `main.py#run()`
    - removed unnecessary wrapper functions and let Console Commands call
    the relevant methods directly. Made possible by #55 which allows
    Commands to wrap the calling of both async & sync functions.
    - added documentation & improved naming

--- bot.py
+++ bot.py
@@ -3,47 +3,24 @@ import asyncio
 from typing import Any
 
 import discord
-from discord import Intents
+from discord import Intents 
 
 from YTDL import YTDLSource
 from console import Console, Command, StringArgsCommand, IntArgCommand
 from playlist import MusicQueue, ExhaustedException
 
 
-class ConsoleClient(discord.Client):
+class MusicClient(discord.Client):
     """A Discord Client that is controllable by the host via a Console."""
 
-    def __init__(self, *, input_method: callable, intents: Intents, **options: Any):
+    def __init__(self, *, intents: Intents, **options: Any):
         super().__init__(intents=intents, **options)
-        self.console = Console(input_method=input_method)
-        self.build_console()
         self.text_channels = []
         self.voice_channels = []
         self.voice_client = None
         self.player = None
         self.playlist = MusicQueue()
         self.VOLUME = 0.5
-
-    def build_console(self):
-        self.console.add_command(StringArgsCommand("play", self.play_now))
-        self.console.add_command(Command("pause", self.pause))
-        self.console.add_command(Command("resume", self.resume))
-        self.console.add_command(Command("stop", self.stop))
-        self.console.add_command(IntArgCommand("volume", self.set_volume))
-        self.console.add_command(Command("channels", self.get_voice_channels))
-        self.console.add_command(IntArgCommand("join", self.join_channel))
-        self.console.add_command(Command("leave", self.leave_channel))
-        self.console.add_command(Command("quit", self.quit))
-        # Enhanced Commands
-        self.console.add_command(StringArgsCommand("queue", self.queue))
-        self.console.add_command(Command("clear", self.clear_queue))
-        self.console.add_command(Command("skip", self.skip_song))
-        self.console.add_command(Command("prev", self.prev_song))
-        self.console.add_command(Command("shuffle", self.playlist_shuffle))
-        self.console.add_command(Command("loop", self.playlist_loop))
-        self.console.add_command(Command("repeat", self.playlist_repeat))
-        self.console.add_command(Command("start", self.start_playing))
-        self.console.add_command(Command("normal", self.playlist_normal))
 
     def load_voice_channels(self):
         for guild in self.guilds:
@@ -58,19 +35,42 @@ class ConsoleClient(discord.Client):
     async def on_ready(self):
         self.load_voice_channels()
         self.load_text_channels()
-        await self.console.run()
 
     async def quit(self):
-        print("[INFO] Shutting down...")
-        self.console.online = False
-        await self.leave_channel()
+        """Stops the MusicClient and shuts it down."""
+        
+        print("Shutting down...")
+        await self.voice_leave()
         await self.close()
 
+    # Audio Streaming Logic
+    async def stream_youtube_url(self, url):
+        """Plays a YouTube URL"""
+        async with self.text_channels[len(self.text_channels) - 1].typing():
+            self.player = await YTDLSource.from_url(url=url, loop=self.voice_client.loop, stream=True)
+            self.player.volume = self.VOLUME
+            self.voice_client.play(self.player,
+                                   after=lambda e: asyncio.run_coroutine_threadsafe(self.stream_next(e),
+                                                                                    self.loop))
+
+    async def stream_next(self, error=None):
+        """Callback function of bot#play which is used to play through the
+        songs in queue."""
+        if error:
+            print(f'Player error: {error}')
+        else:
+            try:
+                url = self.playlist.next()
+                await self.stream_youtube_url(url)
+            except ExhaustedException:
+                print("No more songs.")
+
+    # Voice Channel Controls
     def get_voice_channels(self):
         for index, channel in enumerate(self.voice_channels):
             print(f"[{index}] - {channel}")
 
-    async def join_channel(self, channel_index: int):
+    async def voice_join(self, channel_index: int):
         if self.voice_client is not None:
             await self.voice_client.move_to(self.voice_channels[channel_index])
             return
@@ -80,17 +80,35 @@ class ConsoleClient(discord.Client):
         else:
             print("[WARNING] Invalid channel index!")
 
-    async def leave_channel(self):
+    async def voice_leave(self):
         if self.voice_client is not None:
             self.voice_client.stop()
             await self.voice_client.disconnect()
             self.voice_client = None
 
-    async def play_now(self, urls: list[str]):
-        """Overrides the queue with a new selection of songs, playing them immediately."""
+    # Playlist Controls
+    def playlist_queue(self, urls: list[str]):
+        """Add songs to the playlist."""
+        for url in urls:
+            self.playlist.add(url)
 
-        self.clear_queue()
-        self.queue(urls)
+    async def playlist_start(self):
+        """Starts the playlist."""
+        if self.voice_client is not None:
+            await self.stream_next()
+
+    def playlist_stop(self):
+        """Stops the playlist."""
+        
+        if self.voice_client is not None:
+            self.playlist.clear()
+            self.voice_client.stop()
+
+    async def playlist_play(self, urls: list[str]):
+        """Overrides the Playlist with new songs, playing them"""
+
+        self.playlist.clear()
+        self.playlist_queue(urls)
         
         if self.voice_client is None:
             return
@@ -99,46 +117,45 @@ class ConsoleClient(discord.Client):
             self.voice_client.stop()    # Stops current AudioSource & play_next() callback triggers.
             return
         
-        await self.start_playing()
+        await self.stream_next()
 
-    async def play(self, url):
-        """Plays a YouTube URL"""
-        async with self.text_channels[len(self.text_channels) - 1].typing():
-            self.player = await YTDLSource.from_url(url=url, loop=self.voice_client.loop, stream=True)
-            self.player.volume = self.VOLUME
-            self.voice_client.play(self.player,
-                                   after=lambda e: asyncio.run_coroutine_threadsafe(self.play_next(e),
-                                                                                    self.loop))
 
-    async def play_next(self, error=None):
-        """Callback function of bot#play which is used to play through the
-        songs in queue."""
-        if error:
-            print(f'Player error: {error}')
-        else:
-            try:
-                url = self.playlist.next()
-                await self.play(url)
-            except ExhaustedException:
-                print("No more songs.")
-
-    def queue(self, urls: list[str]):
-        for url in urls:
-            self.playlist.add(url)
-
-    def pause(self):
+    # Audio Controls
+    def audio_pause(self):
+        """Pauses the audio streaming."""
+        
         if self.voice_client is not None:
             self.voice_client.pause()
 
-    def resume(self):
+    def audio_resume(self):
+        """Resumes the audio streaming."""
+        
         if self.voice_client is not None:
             self.voice_client.resume()
 
-    def skip_song(self):
+    def set_audio_volume(self, volume: int):
+        """Set the MusicClient's audio volume level."""
+        
+        volume = float(volume)
+        volume /= 100
+        if not (0.0 <= volume <= 1.0):
+            print("[WARNING] Invalid volume level!")
+            return
+        
+        if self.player is not None:
+            self.player.volume = volume    
+        self.VOLUME = volume
+
+    # Song Controls
+    def song_skip(self):
+        """Play the next song in the playlist."""
+        
         if self.voice_client is not None:
             self.voice_client.stop()
 
-    def prev_song(self):
+    def song_prev(self):
+        """Play the previous song in the playlist."""
+        
         if self.voice_client is not None:
             try:
                 self.playlist.add_first(self.playlist.prev())
@@ -146,45 +163,66 @@ class ConsoleClient(discord.Client):
             except ExhaustedException:
                 print("No more songs")
 
-    def stop(self):
-        if self.voice_client is not None:
-            self.clear_queue()
-            self.voice_client.stop()
 
-    async def start_playing(self):
-        if self.voice_client is not None:
-            await self.play_next()
+def build_client() -> MusicClient:
+    """Builds a MusicClient with necessary correct discord intents.
 
-    def clear_queue(self):
-        self.playlist.clear()
-
-    def set_volume(self, volume: int):
-        volume = float(volume)
-        volume /= 100
-        if 0.0 <= volume <= 1.0:
-            self.VOLUME = volume
-            if self.player is not None:
-                self.player.volume = self.VOLUME
-        else:
-            print("[WARNING] Invalid volume level!")
-
-    def playlist_shuffle(self):
-        self.playlist.shuffle_mode()
-
-    def playlist_normal(self):
-        self.playlist.default_mode()
-
-    def playlist_loop(self):
-        self.playlist.loop_mode()
-
-    def playlist_repeat(self):
-        self.playlist.repeat_mode()
-
-
-
-def run(token: str, input_method: callable):
+    Returns:
+        MusicClient: MusicClient that can be started with `.start(token=token)`.
+    """
+    
     intents = discord.Intents.default()
     intents.message_content = True
+    return MusicClient(intents=intents)
 
-    client = ConsoleClient(intents=intents, input_method=input_method)
-    client.run(token)
+
+def __build_console_commands(console:Console, client:MusicClient): 
+    """Builds the Commands to control a MusicClient to this Console.
+
+    Args:
+        console (Console): Console to add Commands to.
+        client (MusicClient): MusicClient this Console should control. 
+    """
+      
+    # Primary Controls
+    async def quit():
+        """Shuts down the Console and the Client."""
+        console.online = False
+        await client.quit()
+    console.add_command(Command("quit", quit))
+    # Voice Channel Controls
+    console.add_command(Command("channels", client.get_voice_channels))
+    console.add_command(IntArgCommand("join", client.voice_join))
+    console.add_command(Command("leave", client.voice_leave))
+    # Audio Controls
+    console.add_command(Command("pause", client.audio_pause))
+    console.add_command(Command("resume", client.audio_resume))
+    console.add_command(IntArgCommand("volume", client.set_audio_volume))
+    # Song Controls
+    console.add_command(Command("skip", client.song_skip))
+    console.add_command(Command("prev", client.song_prev))
+    # Playlist Controls
+    console.add_command(StringArgsCommand("queue", client.playlist_queue))
+    console.add_command(Command("start", client.playlist_start))
+    console.add_command(Command("stop", client.playlist_stop))
+    console.add_command(Command("clear", client.playlist.clear))
+    console.add_command(StringArgsCommand("play", client.playlist_play))
+    # Playlist Mode Controls
+    console.add_command(Command("shuffle", client.playlist.shuffle_mode))
+    console.add_command(Command("loop", client.playlist.loop_mode))
+    console.add_command(Command("repeat", client.playlist.repeat_mode))
+    console.add_command(Command("normal", client.playlist.default_mode))
+
+
+def build_console(client:MusicClient) -> Console:
+    """Builds a Console for this MusicClient.
+
+    Args:
+        client (MusicClient): MusicClient for this Console to control.
+
+    Returns:
+        Console: Console that can control this MusicClient.
+    """
+    console = Console()
+    __build_console_commands(console, client)
+    return console
