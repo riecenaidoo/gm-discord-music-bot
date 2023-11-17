44604f6b127c0d5267e060e7ef707a785fdc3194
riecenaidoo <riecenaidoo@outlook.com>
2023-11-17 13:50:47 +0200

    formatting utils

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,24 @@
-"""Shared methods."""
+"""Commonly used methods and decorators.
+
+Contains the default logging formatter and handler for the project.
+"""
+
+import asyncio
+import functools
 import logging
 import typing
-import functools
-import asyncio
 
 
-FORMATTER = logging.Formatter(fmt="%(asctime)s %(levelname)-8s [%(funcName)s() in %(name)s:%(lineno)s] %(message)s",
-                  datefmt="%H:%M:%S")
 
+FORMATTER = logging.Formatter(fmt="%(asctime)s %(levelname)-8s"
+                              +" [%(funcName)s() in %(name)s:%(lineno)s] %(message)s",
+                              datefmt="%H:%M:%S")
 HANDLER = logging.StreamHandler()
 HANDLER.setFormatter(FORMATTER)
 
 
 def to_thread(func: typing.Callable):
+    """Wrap a function to run asynchronously."""
     @functools.wraps(func)
     async def wrapper(*args, **kwargs):
         return await asyncio.to_thread(func, *args, **kwargs)
