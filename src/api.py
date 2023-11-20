f7bb9860e6e24cf8ca80e18a649bd96df6fc4873
cuswan022 <cuswan022@student.wethinkcode.co.za>
2023-11-20 13:49:38 +0200

    linted

--- src/api.py
+++ src/api.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 from flask import request
 import asyncio
@@ -6,15 +5,19 @@ import functools
 import typing
 from bot.music_client import MusicClient
 
+
 def to_thread(func: typing.Callable):
     @functools.wraps(func)
     async def wrapper(*args, **kwargs):
         return await asyncio.to_thread(func, *args, **kwargs)
 
     return wrapper
-class APIHandler(Flask):
 
-    def __init__(self, client:MusicClient, import_name: str):  # constructor for the APIHAndler that takes
+
+class APIHandler(Flask):
+    def __init__(
+        self, client: MusicClient, import_name: str
+    ):  # constructor for the APIHAndler that takes
         # a client object, so we can use it for multiple functions/clients IE. DiceRoller and the Music Player
         super().__init__(import_name)
         self.build_command_handler()
@@ -25,16 +28,26 @@ class APIHandler(Flask):
         self.run(host=host, port=port)
 
     def quit():
-        func = request.environ.get('werkzeug.server.shutdown')
+        func = request.environ.get("werkzeug.server.shutdown")
         if func is None:
-            raise RuntimeError('Not running with the Werkzeug Server')
+            raise RuntimeError("Not running with the Werkzeug Server")
         func()
 
     def build_command_handler(self):
         self.add_url_rule("/command/channel", view_func=self.get_channels)
-        self.add_url_rule("/command/volume/<int:volume_number>", view_func=self.set_volume, methods=["POST"])
-        self.add_url_rule("/command/join/<int:channel_number>", view_func=self.join_channel, methods=["POST"])
-        self.add_url_rule("/command/leave", view_func=self.leave_channel, methods=["POST"])
+        self.add_url_rule(
+            "/command/volume/<int:volume_number>",
+            view_func=self.set_volume,
+            methods=["POST"],
+        )
+        self.add_url_rule(
+            "/command/join/<int:channel_number>",
+            view_func=self.join_channel,
+            methods=["POST"],
+        )
+        self.add_url_rule(
+            "/command/leave", view_func=self.leave_channel, methods=["POST"]
+        )
         self.add_url_rule("/command/pause", view_func=self.pause, methods=["POST"])
         self.add_url_rule("/command/resume", view_func=self.resume, methods=["POST"])
         self.add_url_rule("/command/skip", view_func=self.skip)
@@ -49,73 +62,77 @@ class APIHandler(Flask):
         self.add_url_rule("/command/repeat", view_func=self.repeat)
         self.add_url_rule("/command/no_loop", view_func=self.no_loop)
 
-
     def get_channels(self):
         return str(self.client.voice_channels)
-    
 
     def leave_channel(self):
         asyncio.run_coroutine_threadsafe(self.client.voice_leave(), self.client.loop)
         return "Left Channel"
 
     def join_channel(self, channel_number):
-        asyncio.run_coroutine_threadsafe(self.client.voice_join(channel_number), self.client.loop)
+        asyncio.run_coroutine_threadsafe(
+            self.client.voice_join(channel_number), self.client.loop
+        )
         return "Joined Channel"
 
     def pause(self):
         asyncio.run_coroutine_threadsafe(self.client.audio_pause(), self.client.loop)
         return "Paused"
-    
+
     def resume(self):
         asyncio.run_coroutine_threadsafe(self.client.audio_resume(), self.client.loop)
         return "Resumed"
-    
-    def set_volume(self, volume_number:int):
-        volume_number = request.args.get('volume_number')
-        asyncio.run_coroutine_threadsafe(self.client.set_audio_volume(volume_number), self.client.loop)
+
+    def set_volume(self, volume_number: int):
+        volume_number = request.args.get("volume_number")
+        asyncio.run_coroutine_threadsafe(
+            self.client.set_audio_volume(volume_number), self.client.loop
+        )
         return "Volume has been set"
 
     def skip(self):
         asyncio.run_coroutine_threadsafe(self.client.song_skip(), self.client.loop)
         return "Skipped song"
-    
+
     def previous(self):
         asyncio.run_coroutine_threadsafe(self.client.song_prev(), self.client.loop)
         return "Returning to Previous Song"
-    
+
     def playlist_queue(self):
-        songs = request.json['songs']
+        songs = request.json["songs"]
         self.client.playlist_queue(songs)
         return "Added songs to the playlist"
-    
+
     def playlist_start(self):
         asyncio.run_coroutine_threadsafe(self.client.playlist_start(), self.client.loop)
         return "Starting Playlist..."
-    
+
     def playlist_stop(self):
         asyncio.run_coroutine_threadsafe(self.client.playlist_stop(), self.client.loop)
         return "Playlist Stopped!"
-    
+
     def playlist_clear(self):
         asyncio.run_coroutine_threadsafe(self.client.clear(), self.client.loop)
         return "Cleared List of Songs"
-    
+
     def playlist_play(self):
         asyncio.run_coroutine_threadsafe(self.client.playlist_play(), self.client.loop)
         return "Playing New Playlist"
-    
+
     def shuffle(self):
         asyncio.run_coroutine_threadsafe(self.client.shuffle_mode(), self.client.loop)
         return "Shuffled Songs"
-    
+
     def loop_songs(self):
         asyncio.run_coroutine_threadsafe(self.client.loop_mode(), self.client.loop)
         return "Looping All Songs"
-    
+
     def repeat(self):
         asyncio.run_coroutine_threadsafe(self.client.repeat_mode(), self.client.loop)
         return "Repeating Song..."
-    
+
     def no_loop(self):
-        asyncio.run_coroutine_threadsafe(self.client.no_looping_mode(), self.client.loop)
+        asyncio.run_coroutine_threadsafe(
+            self.client.no_looping_mode(), self.client.loop
+        )
         return "Normal Play Resuming"
